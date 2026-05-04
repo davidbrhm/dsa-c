d03d5f2e23ed4db358594aef4a974f0b2f74c280
David95406 <83376469+davidbrhm@users.noreply.github.com>
2026-05-04 22:29:33 +0200

    feat(leetcode): Number of Students Unable to Eat Lunch

---
+++ src/leetcode/1700_number_of_students_unable_to_eat_lunch/solution.c
@@ -0,0 +1,44 @@
+//
+// Created by Ábrahám Dávid on 2026. 05. 02..
+//
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../../include/circular_queue.h"
+
+void init_cq(CircularQueue *cq, int *arr, int n) {
+    cq->elements = malloc((n + 1) * sizeof(int));
+    memcpy(cq->elements, arr, n * sizeof(int));
+
+    cq->capacity = n + 1;
+    cq->front = 0;
+    cq->rear = n - 1;
+}
+
+int cq_first(CircularQueue *cq) {
+    return cq->elements[cq->front];
+}
+
+int countStudents(int *students, int studentsSize, int *sandwiches, int sandwichesSize) {
+    CircularQueue cqueue;
+    init_cq(&cqueue, students, studentsSize);
+
+    int max_lim = studentsSize;
+    int top_sw = 0;
+
+    while (max_lim > 0 && top_sw < sandwichesSize) {
+        if (cq_first(&cqueue) == sandwiches[top_sw]) {
+            top_sw++;
+            cqueue_dequeue(&cqueue);
+            max_lim = studentsSize - top_sw;
+        } else {
+            cqueue_enqueue(&cqueue, cqueue_dequeue(&cqueue));
+            max_lim--;
+        }
+
+    }
+
+    destroy_cqueue(&cqueue);
+    return sandwichesSize - top_sw;
+}
