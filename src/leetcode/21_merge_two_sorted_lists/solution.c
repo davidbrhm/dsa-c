d4bebcb08567c0cf0babbb3db222651ff2ecc3d0
David95406 <83376469+davidbrhm@users.noreply.github.com>
2026-05-09 21:41:51 +0200

    feat(leetcode): merge_two_sorted_lists

---
+++ src/leetcode/21_merge_two_sorted_lists/solution.c
@@ -0,0 +1,44 @@
+//
+// Created by Ábrahám Dávid on 2026. 05. 09..
+//
+
+#include <stddef.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2) {
+    if (list1 == NULL) return list2;
+    if (list2 == NULL) return list1;
+
+    struct ListNode *head;
+    if (list1->val <= list2->val) {
+        head = list1;
+        list1 = list1->next;
+    } else {
+        head = list2;
+        list2 = list2->next;
+    }
+
+    struct ListNode *current = head;
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->val <= list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        } else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+
+        current = current->next;
+    }
+
+    if (list1 != NULL)
+        current->next = list1;
+    else
+        current->next = list2;
+
+    return head;
+}
