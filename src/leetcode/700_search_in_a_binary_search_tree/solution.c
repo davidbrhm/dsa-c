65a49dc8e3b707c417b950c9aa2d858647189eb0
David95406 <83376469+davidbrhm@users.noreply.github.com>
2026-05-18 20:19:59 +0200

    feat(leetcode): search_in_a_binary_search_tree

---
+++ src/leetcode/700_search_in_a_binary_search_tree/solution.c
@@ -0,0 +1,18 @@
+//
+// Created by Ábrahám Dávid on 2026. 05. 18..
+//
+
+#include "../../../include/_leetcode.h"
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    struct TreeNode *curr = root;
+    while (curr != NULL && curr->val != val) {
+        if (curr->val > val) {
+            curr = curr->left;
+        } else {
+            curr = curr->right;
+        }
+    }
+
+    return curr;
+}
