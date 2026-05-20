29d7f5cb076c2b22829537cb2f82d41238ce3630
David95406 <83376469+davidbrhm@users.noreply.github.com>
2026-05-20 19:19:56 +0200

    feat(algorithms): search

---
+++ include/search.h
@@ -0,0 +1,11 @@
+//
+// Created by Ábrahám Dávid on 2026. 05. 20..
+//
+
+#ifndef DSA_C_SEARCH_H
+#define DSA_C_SEARCH_H
+
+int linear_search(const int *arr, int size, int target);
+int binary_search(const int *arr, int size, int target);
+
+#endif //DSA_C_SEARCH_H
---
+++ src/algorithms/search.c
@@ -0,0 +1,38 @@
+//
+// Created by Ábrahám Dávid on 2026. 05. 20..
+//
+
+#include "../../include/search.h"
+
+int linear_search(const int *arr, int size, int target) {
+    if (!arr || size <= 0) return -1;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int binary_search(const int *arr, int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    int center = left + (right - left) / 2;
+
+    while (left <= right) {
+        if (arr[center] > target) {
+            right = center - 1;
+        } else if (arr[center] < target) {
+            left = center + 1;
+        } else {
+            return center;
+        }
+
+        center = (left + right) / 2;
+    }
+
+    return -1;
+}
+
